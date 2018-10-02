c9a57e8d5aa2e3949dd3f7fe276e902c9002354d
Vishwas <gopinav5@gmail.com>
2018-10-02 10:24:53 +0530

    Commit JSX

--- React Fundamentals/components-demo/src/components/Hello.js
+++ React Fundamentals/components-demo/src/components/Hello.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Hello = () => {
   return (
-    <div>
+    <div className='dummyClass'>
       <h1>Hello Vishwas</h1>
     </div>
   )
+
+  // return React.createElement(
+  //   'div',
+  //   {id: 'hello', className: 'dummyClass'},
+  //   React.createElement('h1', null, 'Hello Vishwas')
+  // )
 }
+
 export default Hello
