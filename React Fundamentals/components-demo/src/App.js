64af1bb9e58d029a8e251b3b6c1560fd001d0e01
Vishwas <gopinav5@gmail.com>
2018-10-02 09:08:19 +0530

    Hello component with JSX

--- React Fundamentals/components-demo/src/App.js
+++ React Fundamentals/components-demo/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Hello />
         <Greet name="Bruce" heroName="Batman" />
         <Greet name="Clark" heroName="Superman" />
         <Greet name="Diana" heroName="Wonder Woman" />
---
+++ React Fundamentals/components-demo/src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello Vishwas</h1>
+    </div>
+  )
+}
+export default Hello
