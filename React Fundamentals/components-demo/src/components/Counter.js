1690dad37285a424d61a664d2656798587ce78bd
Vishwas <gopinav5@gmail.com>
2018-10-07 15:00:28 +0530

    Commit setState

--- React Fundamentals/components-demo/src/components/Counter.js
+++ React Fundamentals/components-demo/src/components/Counter.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment() {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }))
+    // this.setState({
+    //   count: this.state.count + 1
+    // }, () => {
+    //   console.log('Callback', this.state.count)
+    // })
+    // this.state.count = this.state.count + 1
+    // console.log(this.state.count)
+  }
+
+  incrementFive() {
+    console.log('Inside incrementFive')
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+  }
+
   render() {
     return (
       <div>
-        Counter
+        <div>Count - {this.state.count}</div>
+        <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     )
   }
