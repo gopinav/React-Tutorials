3c52077189219f5e5c4fb5309d493811f71cd416
Vishwas <gopinav5@gmail.com>
2018-11-01 09:56:20 +0530

    ternary operator approach

--- React Fundamentals/components-demo/src/components/UserGreeting.js
+++ React Fundamentals/components-demo/src/components/UserGreeting.js
@@ -5,16 +5,36 @@ class UserGreeting extends Component {
     super(props)
 
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: true
     }
   }
 
+  // #if-else approach
+  // render() {
+  //   if (this.state.isLoggedIn) {
+  //     return <div>Welcome Vishwas</div>
+  //   } else {
+  //     return <div>Welcome Guest</div>
+  //   }
+  // }
+
+  // #element-variables approach
+  // render() {
+  //   let message
+  //   if (this.state.isLoggedIn) {
+  //     message = <div>Welcome Vishwas</div>
+  //   } else {
+  //     message = <div>Welcome Guest</div>
+  //   }
+  //   return <div>{message}</div>
+  // }
+
   render() {
-    if (this.state.isLoggedIn) {
-      return <div>Welcome Vishwas</div>
-    } else {
-      return <div>Welcome Guest</div>
-    }
+    return this.state.isLoggedIn ? (
+      <div>Welcome Vishwas</div>
+    ) : (
+      <div>Welcome Guest</div>
+    )
   }
 }
 
