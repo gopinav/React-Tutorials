ea6d61958b95bc544f98bae9a69f82744c291e73
Vishwas <gopinav5@gmail.com>
2018-11-02 09:55:36 +0530

    List Rendering with map

--- React Fundamentals/components-demo/src/components/NameList.js
+++ React Fundamentals/components-demo/src/components/NameList.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
   const names = ['Bruce', 'Clark', 'Diana']
-  return (
-    <div>
-      <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
-    </div>
-  )
+  const persons = [
+    {
+      id: 1,
+      name: 'Bruce',
+      age: 30,
+      skill: 'React'
+    },
+    {
+      id: 2,
+      name: 'Clark',
+      age: 25,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Diana',
+      age: 28,
+      skill: 'Vue'
+    }
+  ]
+  const personList = persons.map(person => <Person person={person} />)
+  return <div>{personList}</div>
 }
 
 export default NameList
---
+++ React Fundamentals/components-demo/src/components/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Person({person}) {
+  return (
+    <div>
+      <h2>
+        I am {person.name}. I am {person.age} years old. I know {person.skill}
+      </h2>
+    </div>
+  )
+}
+
+export default Person
