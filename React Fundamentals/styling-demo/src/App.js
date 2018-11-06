954ce0b0d0f3cad240936a0b0a1711761d2ea898
Vishwas <gopinav5@gmail.com>
2018-11-06 18:34:58 +0530

    Inline styling

--- React Fundamentals/styling-demo/src/App.js
+++ React Fundamentals/styling-demo/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import Stylesheet from './components/Stylesheet';
+import React, { Component } from 'react'
+import './App.css'
+import Stylesheet from './components/Stylesheet'
+import Inline from './components/Inline'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Stylesheet primary={true} />
+        <Inline />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ React Fundamentals/styling-demo/src/components/Inline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const heading = {
+  fontSize: '72px',
+  color: 'blue'
+}
+
+function Inline() {
+  return (
+    <div>
+      <h1 style={heading}>Inline</h1>
+    </div>
+  )
+}
+
+export default Inline
