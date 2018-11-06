1c988da6f72a1cc1ef6b7adbdd9e3e50b691826b
Vishwas <gopinav5@gmail.com>
2018-11-06 17:23:57 +0530

    Index as key

--- React Fundamentals/components-demo/src/components/NameList.js
+++ React Fundamentals/components-demo/src/components/NameList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana']
+  const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
   const persons = [
     {
       id: 1,
@@ -24,7 +24,8 @@ function NameList() {
     }
   ]
   const personList = persons.map(person => <Person key={person.id} person={person} />)
-  return <div>{personList}</div>
+  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+  return <div>{nameList}</div>
 }
 
 export default NameList
