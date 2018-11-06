97b440a8f3b7264794458e2a739053b8a4515be2
Vishwas <gopinav5@gmail.com>
2018-11-06 19:27:12 +0530

    CSS Modules

--- React Fundamentals/styling-demo/src/App.js
+++ React Fundamentals/styling-demo/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './App.css'
 import Stylesheet from './components/Stylesheet'
 import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 class App extends Component {
   render() {
@@ -9,6 +11,8 @@ class App extends Component {
       <div>
         <Stylesheet primary={true} />
         <Inline />
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
       </div>
     )
   }
--- React Fundamentals/styling-demo/src/components/Inline.js
+++ React Fundamentals/styling-demo/src/components/Inline.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const heading = {
   fontSize: '72px',
   color: 'blue'
