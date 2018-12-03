e05cdc38513a07408074440535e1170372b32fd9
Vishwas <gopinav5@gmail.com>
2018-12-03 20:49:02 +0530

    Ref to get value

--- React Fundamentals/advanced-guide-demo/src/components/RefsDemo.js
+++ React Fundamentals/advanced-guide-demo/src/components/RefsDemo.js
@@ -11,10 +11,15 @@ export class RefsDemo extends Component {
 		console.log(this.inputRef)
 	}
 
+	clickHandler = () => {
+		alert(this.inputRef.current.value)
+	}
+
 	render() {
 		return (
 			<div>
 				<input type="text" ref={this.inputRef} />
+				<button onClick={this.clickHandler}>Click</button>
 			</div>
 		)
 	}
