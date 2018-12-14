aafc4c018fdbbe9d410049ee967a8c28b8ee62e1
Vishwas <gopinav5@gmail.com>
2018-12-14 20:48:42 +0530

    PC Initial Commit

---
+++ React Fundamentals/advanced-guide-demo/src/components/PureComp.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class PureComp extends PureComponent {
+  render() {
+    return (
+      <div>
+        Pure Component
+      </div>
+    )
+  }
+}
+
+export default PureComp
