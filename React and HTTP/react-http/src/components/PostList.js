5df1ca09d70b8eef435b390de1e91c17bc223724
Vishwas <gopinav5@gmail.com>
2019-02-09 20:12:02 +0530

    Commit get request

---
+++ React and HTTP/react-http/src/components/PostList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+class PostList extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+      posts: [],
+      errorMsg: ''
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts')
+			.then(response => {
+				console.log(response)
+				this.setState({ posts: response.data })
+			})
+			.catch(error => {
+        console.log(error)
+        this.setState({errorMsg: 'Error retrieving data'})
+			})
+	}
+
+	render() {
+		const { posts, errorMsg } = this.state
+		return (
+			<div>
+				List of posts
+				{posts.length
+					? posts.map(post => <div key={post.id}>{post.title}</div>)
+          : null}
+        {errorMsg ? <div>{errorMsg}</div> : null}
+			</div>
+		)
+	}
+}
+
+export default PostList
