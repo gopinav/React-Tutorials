ff167673314b27f68f8a9819b580be463bcc8e1a
Vishwas <gopinav5@gmail.com>
2019-03-31 20:27:41 +0530

    useState previous state

--- React Hooks/state-hook/src/App.js
+++ React Hooks/state-hook/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
+import React, { Component } from 'react'
+import './App.css'
+import ClassCounter from './components/ClassCounter'
+import HookCounter from './components/HookCounter'
+import HookCounterTwo from './components/HookCounterTwo'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ClassCounter />
-        <HookCounter />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<ClassCounter />
+				<HookCounter />
+				<HookCounterTwo />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- React Hooks/state-hook/src/components/HookCounterTwo.js
+++ React Hooks/state-hook/src/components/HookCounterTwo.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HookCounterTwo() {
-  return (
-    <div>
-
-    </div>
-  )
+	const initialCount = 0
+	const [count, setCount] = useState(initialCount)
+	const incrementFive = () => {
+		for (let i = 0; i < 5; i++) {
+			setCount(prevCount => prevCount + 1)
+		}
+	}
+	return (
+		<>
+			Count: {count}
+			<button onClick={() => setCount(initialCount)}>Reset</button>
+			<button onClick={() => setCount(prevCount => prevCount + 1)}>
+				Increment
+			</button>
+			<button onClick={() => setCount(prevCount => prevCount - 1)}>
+				Decrement
+			</button>
+			<button onClick={incrementFive}>Increment 5</button>
+		</>
+	)
 }
 
 export default HookCounterTwo
