70861cd9fb3b73250c96d2068ddcf35ac83d29f3
Vishwas <gopinav5@gmail.com>
2019-04-06 09:30:25 +0530

    format code

--- React Hooks/state-hook/src/components/HookCounterThree.js
+++ React Hooks/state-hook/src/components/HookCounterThree.js
@@ -4,11 +4,19 @@ function HookCounterThree() {
 	const [name, setName] = useState({ firstName: '', lastName: '' })
 	return (
 		<form>
-      <input onChange={(e) => setName({...name, firstName: e.target.value })} type="text" />
-      <input onChange={(e) => setName({...name, lastName: e.target.value })} type="text" />
-      <h2>Your first name is {name.firstName}</h2>
-      <h2>Your last name is {name.lastName}</h2>
-      <h2>{JSON.stringify(name)}</h2>
+			<input
+				type="text"
+				value={name.firstName}
+				onChange={e => setName({ ...name, firstName: e.target.value })}
+			/>
+			<input
+				type="text"
+				value={name.lastName}
+				onChange={e => setName({ ...name, lastName: e.target.value })}
+			/>
+			<h2>Your first name is - {name.firstName}</h2>
+			<h2>Your last name is - {name.lastName}</h2>
+			<h2>{JSON.stringify(name)}</h2>
 		</form>
 	)
 }
