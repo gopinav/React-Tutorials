f25c45090f6319c65f3a1a46bf313ade7fd3b130
Vishwas <gopinav5@gmail.com>
2019-04-28 14:26:01 +0530

    Conditionally run effects

--- React Hooks/effect-hook/src/App.js
+++ React Hooks/effect-hook/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<ClassCounterOne />
-				{/* <HookCounterOne /> */}
+				<HookCounterOne />
 				{/* <ClassCounterTwo></ClassCounterTwo> */}
 				{/* <HookCounterTwo></HookCounterTwo> */}
 			</div>
--- React Hooks/effect-hook/src/components/HookCounterOne.js
+++ React Hooks/effect-hook/src/components/HookCounterOne.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 
 function HookCounterOne() {
 	const [count, setCount] = useState(0)
+	const [name, setName] = useState('')
 	useEffect(() => {
+		console.log('useEffect - Updating document title ')
 		document.title = `You clicked ${count} times`
-	})
+	}, [count])
 	return (
 		<div>
+			<input type="text" value={name} onChange={e => setName(e.target.value)} />
 			<button onClick={() => setCount(count + 1)}>
-				useEffct - Click {count} times
+				useEffect - Click {count} times
 			</button>
 		</div>
 	)
