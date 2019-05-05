9e304cf00979bf270f9b5f546fb58970fd74a78e
Vishwas <gopinav5@gmail.com>
2019-05-05 12:14:41 +0530

    Data fetching with useEffect

--- React Hooks/effect-hook/src/App.js
+++ React Hooks/effect-hook/src/App.js
@@ -5,8 +5,9 @@ import HookCounterOne from './components/HookCounterOne'
 import ClassMouse from './components/ClassMouse'
 import HookMouse from './components/HookMouse'
 import MouseContainer from './components/MouseContainer'
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
+import IntervalClassCounter from './components/IntervalClassCounter'
+import IntervalHookCounter from './components/IntervalHookCounter'
+import DataFetching from './components/DataFetching'
 
 class App extends Component {
 	render() {
@@ -17,8 +18,9 @@ class App extends Component {
 				{/* <ClassMouse></ClassMouse> */}
 				{/* <HookMouse></HookMouse> */}
 				{/* <MouseContainer /> */}
-				<IntervalClassCounter></IntervalClassCounter>
-				<IntervalHookCounter></IntervalHookCounter>
+				{/* <IntervalClassCounter></IntervalClassCounter> */}
+				{/* <IntervalHookCounter></IntervalHookCounter> */}
+				<DataFetching />
 			</div>
 		)
 	}
--- React Hooks/effect-hook/src/components/DataFetching.js
+++ React Hooks/effect-hook/src/components/DataFetching.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function DataFetching() {
-  return (
-    <div>
+	const [posts, setPosts] = useState([])
 
-    </div>
-  )
+	useEffect(() => {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts')
+			.then(res => {
+        console.log(res)
+        setPosts(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
+	return (
+		<div>
+			<ul>
+				{posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default DataFetching
