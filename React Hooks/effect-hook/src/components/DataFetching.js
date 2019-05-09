4fc14d611f73439c9cf067fb77e42505280f19bd
Vishwas <gopinav5@gmail.com>
2019-05-09 18:45:30 +0530

    Data fetch on character change

--- React Hooks/effect-hook/src/components/DataFetching.js
+++ React Hooks/effect-hook/src/components/DataFetching.js
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-	const [posts, setPosts] = useState([])
+	const [post, setPost] = useState({})
+	const [id, setId] = useState(1)
 
 	useEffect(() => {
 		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
+			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
 			.then(res => {
         console.log(res)
-        setPosts(res.data)
+        setPost(res.data)
 			})
 			.catch(err => {
 				console.log(err)
 			})
-	}, [])
+	}, [id])
 	return (
 		<div>
-			<ul>
+			<input type="text" value={id} onChange={e => setId(e.target.value)} />
+			<div>{post.title}</div>
+			{/* <ul>
 				{posts.map(post => (
           <li key={post.id}>{post.title}</li>
 				))}
-			</ul>
+			</ul> */}
 		</div>
 	)
 }
