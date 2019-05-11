4bcbbbff05eb69c04579a376723b8bc9f79a2a23
Vishwas <gopinav5@gmail.com>
2019-05-11 18:19:47 +0530

    context hook

--- React Hooks/context-hook/src/components/ComponentE.js
+++ React Hooks/context-hook/src/components/ComponentE.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ComponentF from './ComponentF'
+import { UserContext, ChannelContext } from '../App'
 
 function ComponentE() {
-	return <ComponentF />
+
+  const user = useContext(UserContext)
+  const channel = useContext(ChannelContext)
+  return <div> User is {user} and channel is {channel}</div>
 }
 
 export default ComponentE
