5e278aed53b340b305d5d2afc4230e297447cbc0
Vishwas <gopinav5@gmail.com>
2019-06-22 12:27:48 +0530

    useReducer with useContext

--- React Hooks/reducer-hook/src/App.js
+++ React Hooks/reducer-hook/src/App.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css'
 import CounterOne from './components/CounterOne'
 import CounterTwo from './components/CounterTwo'
 import CounterThree from './components/CounterThree'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+
+const initialState = 0
+const reducer = (state, action) => {
+	switch (action) {
+		case 'increment':
+			return state + 1
+		case 'decrement':
+			return state - 1
+		case 'reset':
+			return initialState
+		default:
+			return state
+	}
+}
+
+export const CountContext = React.createContext()
 
 function App() {
+	const [count, dispatch] = useReducer(reducer, initialState)
 	return (
-		<div className="App">
-			{/* <CounterOne /> */}
-			{/* <CounterTwo /> */}
-			<CounterThree />
-		</div>
+		<CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
+			<div className="App">
+				{/* <CounterOne /> */}
+				{/* <CounterTwo /> */}
+				{/* <CounterThree /> */}
+				{count}
+				<ComponentA />
+				<ComponentB />
+				<ComponentC />
+			</div>
+		</CountContext.Provider>
 	)
 }
 
--- React Hooks/reducer-hook/src/components/ComponentD.js
+++ React Hooks/reducer-hook/src/components/ComponentD.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { CountContext } from '../App';
 
 function ComponentD() {
+  const countContext = useContext(CountContext)
   return (
     <div>
-      Component D
+      Component D {countContext.countState}
+      <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
+			<button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
+			<button onClick={() => countContext.countDispatch('reset')}>Reset</button>
     </div>
   )
 }
