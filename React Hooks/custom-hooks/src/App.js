a21319064f4dceb266552d0127a06fbdc163f4b6
Vishwas <gopinav5@gmail.com>
2019-07-14 14:22:57 +0530

    Counter

--- React Hooks/custom-hooks/src/App.js
+++ React Hooks/custom-hooks/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import DocTitleOne from './components/DocTitleOne'
 import DocTitleTwo from './components/DocTitleTwo'
+import CounterOne from './components/CounterOne'
 
 function App() {
 	return (
 		<div className="App">
-			<DocTitleOne />
-			<DocTitleTwo />
+			{/* <DocTitleOne /> */}
+			{/* <DocTitleTwo /> */}
+			<CounterOne />
 		</div>
 	)
 }
---
+++ React Hooks/custom-hooks/src/components/CounterOne.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+function CounterOne() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  return (
+    <div>
+      <h2>Count = {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default CounterOne
